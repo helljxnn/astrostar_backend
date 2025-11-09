b73614b67821752ffcf713dcefcfb17b6e4c5b56
Alejandro <alejandrogranadaperez200@gmail.com>
2025-11-09 18:31:29 -0500

    adding the donors-sponsors controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import providerRoutes from "../modules/Providers/routes/providers.routes.js";
 import documentTypesRoutes from "./documentTypes.routes.js";
 import testEmailRoutes from "./testEmail.js";
 import sportsEquipmentRoutes from "../modules/SportsEquipment/routes/sportsEquipment.routes.js";
+import donorsSponsorsRoutes from "../modules/DonorsSponsors/routes/donorsSponsor.routes.js";
 import { authenticateToken } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -23,6 +24,7 @@ router.use("/users", authenticateToken, usersRoutes);
 router.use("/providers", authenticateToken, providerRoutes);
 router.use("/document-types", authenticateToken, documentTypesRoutes);
 router.use("/sportsEquipment", authenticateToken, sportsEquipmentRoutes);
+router.use("/donorsSponsors", authenticateToken, donorsSponsorsRoutes);
 
 // Test routes (only in development)
 if (process.env.NODE_ENV === "development") {
