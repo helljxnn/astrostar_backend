39124204dced825bc052a7076ab82bfa99832926
Jennx-x <jennls.prog@gmail.com>
2026-03-19 18:37:28 -0500

    fix: prisma issues to deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,8 @@
 ﻿import { PrismaClient } from "../../generated/prisma/index.js";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-// Create PostgreSQL adapter
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
 const prisma = new PrismaClient({
-  adapter,
   log: isDevelopment ? ["query", "info", "warn", "error"] : ["warn", "error"],
 });
 
